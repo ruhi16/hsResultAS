d4c3e64f5f92d687fb48387f3cda3c8fda0373f5
ruhi16 <hndas2016@gmail.com>
2019-03-01 00:40:40 +0530

    home

---
+++ database/migrations/2019_02_28_172523_create_sessions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('stdate');
+            $table->date('endate');
+            $table->unsignedinteger('next_session_id')->nullable();
+            $table->boolean('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
